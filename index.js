bac9483c574cbf9fa135d834df0f39841c77b386
Andrew Shini <superandrew213@gmail.com>
2017-10-10 18:56:09 +1000

    Add promise option

---
+++ index.js
@@ -0,0 +1,43 @@
+import {
+  NativeEventEmitter,
+  NativeModules,
+  Platform,
+} from 'react-native';
+
+const { InAppUtils } = NativeModules;
+
+const InAppUtilsEmitter = new NativeEventEmitter(InAppUtils);
+
+const promisify = fn => (...args) => new Promise((resolve, reject) => {
+  fn(...args, (err, res) => err ? reject(err) : resolve(res));
+});
+
+const IAU = Platform.select({
+  ios: {
+    loadProducts: (products, cb) => cb
+      ? InAppUtils.loadProducts(products, cb)
+      : promisify(InAppUtils.loadProducts)(products),
+
+    canMakePayments: cb => cb
+      ? InAppUtils.canMakePayments(cb)
+      : promisify(InAppUtils.canMakePayments)(),
+
+    purchaseProduct: (productIdentifier, cb) => cb
+      ? InAppUtils.purchaseProduct(productIdentifier, cb)
+      : promisify(InAppUtils.purchaseProduct)(productIdentifier),
+
+    restorePurchases: cb => cb
+      ? InAppUtils.restorePurchases(cb)
+      : promisify(InAppUtils.restorePurchases)(),
+
+    receiptData: cb => cb
+    ? InAppUtils.receiptData(cb)
+    : promisify(InAppUtils.receiptData)(),
+
+    addListener: (event, cb) => InAppUtilsEmitter.addListener(event, cb),
+  },
+
+  android: {},
+});
+
+export default IAU;
