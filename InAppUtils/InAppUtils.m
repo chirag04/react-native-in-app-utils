92c8fe7e7e31a77de8a2023df4ed5fc5f4289ae6
Yannick Spark <4729938+YannickDot@users.noreply.github.com>
2019-05-02 16:19:34 +0200

    Added localized price string in introductoryPrice

--- InAppUtils/InAppUtils.m
+++ InAppUtils/InAppUtils.m
@@ -316,6 +316,9 @@ RCT_EXPORT_METHOD(receiptData:(RCTResponseSenderBlock)callback)
             
             NSDictionary *introductoryPrice = @{
                                                 @"price": product.introductoryPrice.price,
+                                                @"currencySymbol": [product.introductoryPrice.priceLocale objectForKey:NSLocaleCurrencySymbol],
+                                                @"currencyCode": [product.introductoryPrice.priceLocale objectForKey:NSLocaleCurrencyCode],
+                                                @"countryCode": [product.introductoryPrice.priceLocale objectForKey: NSLocaleCountryCode],
                                                 @"numberOfPeriods": [[NSNumber alloc] initWithLong:product.introductoryPrice.numberOfPeriods],
                                                 @"paymentMode": paymentMode,
                                                 @"subscriptionPeriod": subscriptionPeriod,
