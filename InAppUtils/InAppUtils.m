33af892fb785a6474a2cc510bd4881dddcd4c7c1
Carlos Garcia <carlos@lingokids.com>
2019-09-13 16:57:20 +0200

    downloadable deprecated code fix

--- InAppUtils/InAppUtils.m
+++ InAppUtils/InAppUtils.m
@@ -235,7 +235,7 @@ RCT_EXPORT_METHOD(receiptData:(RCTResponseSenderBlock)callback)
                                       @"currencyCode": [item.priceLocale objectForKey:NSLocaleCurrencyCode],
                                       @"priceString": item.priceString,
                                       @"countryCode": [item.priceLocale objectForKey: NSLocaleCountryCode],
-                                      @"downloadable": item.downloadable ? @"true" : @"false" ,
+                                      @"downloadable": item.isDownloadable ? @"true" : @"false" ,
                                       @"description": item.localizedDescription ? item.localizedDescription : @"",
                                       @"title": item.localizedTitle ? item.localizedTitle : @"",
                                       };
